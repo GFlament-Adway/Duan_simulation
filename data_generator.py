d56ec825dd2a3283812e6efff162c99a0b9c0996
guillaume.flament_ad <guillaume.flament@dway-conseil.com>
2022-10-31 16:45:17 +0100

    metropolis_sampler

--- data_generator.py
+++ data_generator.py
@@ -1,20 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import lifelines
+import math
+from scipy.stats import halfnorm
 
-def metropolis_sampler(x_0, s, t, n_sample = 1000):
-    #Init step, we use gaussian distribution as the proposal density.
-    x_c = np.random.normal(x_0, 1)
-    sample = [x_c]
-    for _ in range(n_sample):
-        x_c = np.random.normal(sample[-1], 1)
-        acceptance = posterior(x_c, s, t)/posterior(sample[-1], s,t)
-        u = np.random.uniform(0, 1)
-        if u <= acceptance:
-            sample += [x_c]
-        else:
-            pass
-    return sample[1:]
 
 def ornstein_uhlenbeck(t, ar):
     U = [0]
@@ -23,13 +12,50 @@ def ornstein_uhlenbeck(t, ar):
     return U
 
 
-def posterior(theta_t, s, t, eta_0 = 1, gamma_0 = 20):
-    post = np.exp(-(s - t) * theta_t) - np.exp(-(s - t + 1) * theta_t) * theta_t ** (eta_0 - 1) * np.exp(
-        -(gamma_0 + t) * theta_t)
+def metropolis_sampler(x_0, s, t, burn_in=50, n_sample=100, gamma_0=20, eta=1):
+    """
+
+    :param x_0:
+    :param s:
+    :param t:
+    :param burn_in:
+    :param n_sample:
+    :return:
+    """
+    # Init step, we use gaussian distribution as the proposal density.
+
+    x_c = np.random.normal(x_0, 1)
+    x_c = np.sign(x_c) * x_c
+    sample = [x_c]
+    n_it = 0
+    while len(sample) < 2:
+        for _ in range(burn_in + n_sample):
+            n_it += 1
+            x_c = np.random.normal(x_0, 1)
+            x_c = np.sign(x_c) * x_c
+            if n_it > 1000:
+                print(x_c, posterior(x_c, s, t, eta, gamma_0), x_0, posterior(x_0, s, t, eta, gamma_0))
+
+            acceptance = posterior(x_c, s, t, eta, gamma_0) / posterior(x_0, s, t, eta, gamma_0)
+            u = np.random.uniform(0, 1)
+            if u <= acceptance and n_it > burn_in:
+                sample += [x_c]
+            else:
+                x_0 = x_c
+    #assert np.sum([s < 0 for s in sample]) == 0, print(np.sum([s < 0 for s in sample]))
+    return sample[1:]
+
+
+
+
+
+def posterior(theta_t, s, t, eta=1, gamma_0=20):
+    post = (np.exp(-(s - t) * theta_t) - np.exp(-(s - t + 1) * theta_t)) * np.exp(
+        -(gamma_0 + t) * theta_t) * theta_t ** (eta - 1)
     return post
 
 
-def get_data(n_ind=100, n_times=50, p=3, gamma_0=20, eta=1):
+def get_data(n_ind=100, n_times=50, gamma_0=40, eta=1, tau=0):
     """
     :param n_ind:
     :param n_times:
@@ -37,49 +63,86 @@ def get_data(n_ind=100, n_times=50, p=3, gamma_0=20, eta=1):
     :param eta:
     :return:
     """
-
-    alpha = [1, 1, 1, -0.5]
+    alpha = [[-5, 1] for _ in range(tau + 1)]
     C = []
     delta = []
     Times = []
-    Z = []
-    p_1, p_2 = np.random.multivariate_normal([1, 1], [[0.5, 0.2], [0.2, 0.5]])
-    eta_0 = 0.8
-
+    eta_0 = 0
     mu = [0]
+    Z = []
+    S = []
 
-    for i in range(n_ind):
-        Z_temp = []
-        Z_temp += [[p_1 + p_2 * t + np.random.normal(0, 1) for t in range(n_times)]]
-        Z_temp += [[t + np.random.normal() for t in range(5)] + [3 * t - 10 + np.random.normal() for t in range(5, n_times)]]
-        U = ornstein_uhlenbeck(n_times, 0.5)
-        for t in range(1, n_times):
-            mu += [t + (mu[t - 1] * 0.2 + np.random.normal())]
-        Z_temp += [[t + mu[t] for t in range(n_times)]]
+    if tau == 0:
+        for i in range(n_ind):
+            p_1 = np.random.normal(0, 1)
+            p_2 = np.random.normal(0, 1)
+            Z_temp = []
+            Z_temp += [[1 if p_1 < 0 else 0 for _ in range(n_times)]]
+            Z_temp += [[1 if p_2 < 0 else -1 for _ in range(n_times)]]
+            """
+            Z_temp += [
+                [np.random.normal(-1,2) for t in range(5)] + [np.random.normal(-5, 3)
+                                                                           for t in range(5, n_times)]]
+            Z_temp += [
+                [np.random.normal(7, 6) for t in range(5)] + [ np.random.normal(10,3)
+                                                                           for t in range(5, n_times)]]
+            U = ornstein_uhlenbeck(n_times, 0.5)
+            for t in range(1, n_times):
+                mu += [mu[t - 1] * 0.2 + np.random.normal()]
+            Z_temp += [[mu[t] for t in range(n_times)]]
+            """
+            Z += [Z_temp]
+            X = np.matmul(np.array(Z[i]).T, alpha[0])
+            f = np.exp(X)
+            u = np.random.uniform(0, 1)
+            S += [[np.exp(-np.sum([f[:t]])) for t in range(n_times)]]
+            Time = [t for t in range(len(S[i])) if S[i][t] < u or t == n_times - 1][0]
+            c = np.random.exponential(50)
+            C += [c]
+            delta += [1 if (c < Time and Time < n_times) else 0]
+            Times += [min(int(Time), n_times - 1)]
+    else:
+        Z = []
+        p_1, p_2 = np.random.multivariate_normal([1, 1], [[0.5, 0.2], [0.2, 0.5]])
+        eta_0 = 0
 
+        for i in range(n_ind):
+            Z_temp = []
+            Z_temp += [[p_1 + p_2 * np.sqrt(t + 1) + np.random.normal(0, 1) for t in range(n_times)]]
 
-        Time = np.random.gamma(gamma_0, eta)
-        Z_0 = []
-        for t in range(n_times):
-            theta_t = 10 / (gamma_0 + t)
-            Z_0 += [(np.log(theta_t) - np.sum([alpha[k] * Z_temp[k][t] for k in range(p - 1)]) - eta_0 * U[t]) / alpha[-1]]
-        Z_temp += [Z_0]
+            """ 
+            Z_temp += [
+                [np.sqrt(t + 1) + np.random.normal() for t in range(5)] + [3 * np.sqrt(t + 1) - 10 + np.random.normal()
+                                                                           for t in range(5, n_times)]]
+            U = ornstein_uhlenbeck(n_times, 0.5)
+            for t in range(1, n_times):
+                mu += [mu[t - 1] * 0.2 + np.random.normal()]
+            Z_temp += [[np.log(t + 1) + mu[t] for t in range(n_times)]]
+            """
+            Time = np.random.gamma(gamma_0, eta)
+            Z_0 = []
+            for t in range(n_times):
+                print("sampling : ", gamma_0)
+                sample = metropolis_sampler(10 / (gamma_0 + t), Time, t, gamma_0=gamma_0, eta=eta)
+                theta_t = np.random.choice(sample, 1)[0]
+                print(i, Time, t, theta_t)
+                assert len(Z_temp) == len(alpha[0]) - 1, "Not enough alpha"
+                num = (np.log(theta_t) - np.sum(
+                    np.sum([alpha[0][k] * Z_temp[k][t] for k in range(len(Z_temp))])))
+                Z_0 += [num / alpha[0][-1]]
+            Z_temp += [Z_0]
+            Z += [Z_temp]
 
-        Z += [Z_temp]
-        c = np.random.exponential(40)
-        C += [c]
-        delta += [1 if(c < Time and Time < n_times) else 0]
-        Times += [min(int(Time), n_times)]
-    return np.array(Times, dtype=object), np.array(Z, dtype=object), np.array(delta, dtype=object), alpha
+            c = np.random.exponential(70)
+            C += [c]
+            delta += [1 if (c < Time and Time < n_times) else 0]
+            Times += [min(int(Time), n_times - 1)]
+
+    return np.array(Times, dtype=object), np.array(Z, dtype=object), np.array(delta, dtype=object), alpha, S
 
 
 if __name__ == "__main__":
-    n_ind = 200
-    U = ornstein_uhlenbeck(100, 0.4)
-    Times, Z, delta = get_data(n_ind=n_ind)
-
-    ax = plt.figure()
-    ax = lifelines.plotting.plot_lifetimes(durations=Times[:20], event_observed=delta[:20], sort_by_duration=False)
-    ax.set_xlabel("Event times")
+    sample = metropolis_sampler(10, 30, 1)
+    plt.figure()
+    plt.hist(sample, bins=len(sample) // 20)
     plt.show()
-
--- main.py
+++ main.py
@@ -2,34 +2,61 @@ from data_generator import get_data
 from likelihood import Intensity_Model
 import numpy as np
 import matplotlib.pyplot as plt
+import warnings
+
 
 def show_covariates(X):
     plt.figure()
     for i in range(len(X[0][0])):
-        plt.plot(X[:, 0, i], label=r"$X_{i}$".format(i=i))
-        plt.plot(X[:, 3, i], label=r"$X_{i}$".format(i=i))
         plt.plot(X[:, 5, i], label=r"$X_{i}$".format(i=i))
         plt.legend()
-    plt.show()
-
-def verify_theta(X, betas):
-    theta = np.matmul(X, betas)
-    print(np.array(theta).shape)
+    plt.draw()
 
 
 if __name__ == "__main__":
+    # warnings.filterwarnings("ignore")  # Supprime des potentiels warnings dans l'algorithme Metropolis-Hastings.
+    np.random.seed(123456789)
     n_ind = 100
-    n_times = 30
+    n_times = 100
+    tau = 2
+    print("#################################")
+    print("Generating data")
 
-    Times, X, Cens, true_beta = get_data(n_ind=n_ind, n_times=n_times)
-
+    Times, X, Cens, true_beta, surv = get_data(n_ind=n_ind, n_times=n_times, tau=tau, gamma_0=20)
+    print(Times)
+    print(surv)
+    print("True beta : ", true_beta)
+    print(np.array(X).shape)
+    print("mean event time : ", np.mean(Times))
+    print("Censorship rate : ", np.mean(Cens))
     Y = [[0 for _ in range(len(X[0][0]))] for _ in range(len(X))]
-    X = np.array([np.array(X[:,::,i]) for i in range(len(X[0][0]))])
-    verify_theta(X, true_beta)
+    X = np.array([np.array(X[:, ::, i]) for i in range(len(X[0][0]))])
+    #print(np.exp(np.matmul(X, true_beta[0]).T.astype(float)))
+    # verify_theta(X, true_beta)
+    #show_covariates(X)
+    print("True beta : ", true_beta)
 
-    """
-    intensity_model = Intensity_Model(X, Times, Cens, Y, tau = 0)
+    print("#################################")
+    print("Data generated")
+    intensity_model = Intensity_Model(X, Times, Cens, Y, tau=tau)
     print(intensity_model.betas)
-    intensity_model.fit()
-    print(intensity_model.param)
-    """
+    print("likelihood at true beta : ", intensity_model.f_likelihood(true_beta, to_print=True))
+    sigmas = np.linspace(10e-10, 10e-1, num=100)
+    test_inits = [[[np.random.normal(0, sigma) for i in range(len(true_beta[0]))] for k in range(tau + 1)] for sigma in
+                  sigmas]
+
+    print("likelihood at initialization : ", intensity_model.f_likelihood(test_inits[-1]))
+    intensity_model.fit(init=test_inits[-1])
+    print("beta after optim : ", intensity_model.estimated_betas)
+    print("n iteration :", intensity_model.param["nit"])
+    print("likelihood after optim : ", intensity_model.f_likelihood(intensity_model.param["x"]))
+
+    preds = [intensity_model.pred(X[:, i, :], 0) for i in range(len(Times))]
+    true_preds = [intensity_model.pred(X[:, i, :], 0, true_beta=true_beta) for i in range(len(Times))]
+    plt.figure()
+    plt.ylim(0, 1)
+    for i in range(len(Times)):
+        plt.plot(preds[i], color="red", alpha = 0.1)
+        plt.plot(true_preds[i], color="blue", alpha = 0.1)
+        #plt.plot(surv[i], color="blue", alpha = 0.1)
+    plt.show()
